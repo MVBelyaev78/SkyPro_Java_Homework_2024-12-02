0e9e8611d125e7b1e980bc5462e3b0ed731b4a76
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-10 13:58:02 +0300

    first IDEA commit

---
+++ src/main/java/pro/sky/JavaHomework/SkyProJavaHomework20241202Application.java
@@ -0,0 +1,13 @@
+package pro.sky.JavaHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProJavaHomework20241202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProJavaHomework20241202Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/JavaHomework/SkyProJavaHomework20241202ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.JavaHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProJavaHomework20241202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
