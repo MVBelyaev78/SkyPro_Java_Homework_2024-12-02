412209ec1cf3767cf088059380fed339e9d652e9
Mikhail Belyaev <mbelyaev78@gmail.com>
2024-12-13 17:06:03 +0300

    refactoring

--- src/main/java/pro/sky/JavaHomework/services/BasketService.java
+++ src/main/java/pro/sky/JavaHomework/services/BasketService.java
@@ -1,26 +1,8 @@
 package pro.sky.JavaHomework.services;
 
-import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.SessionScope;
-import pro.sky.JavaHomework.components.Basket;
-import pro.sky.JavaHomework.components.BasketImpl;
-
 import java.util.Set;
 
-@Service
-@SessionScope
-public class BasketService {
-    private final Basket basket;
-
-    public BasketService() {
-        this.basket = new BasketImpl();
-    }
-
-    public Set<Integer> getGoods() {
-        return basket.getGoods();
-    }
-
-    public void addGoods(Set<Integer> keys) {
-        keys.forEach(basket::addGoods);
-    }
+public interface BasketService {
+    public Set<Integer> getGoods();
+    public void addGoods(Set<Integer> keys);
 }
---
+++ src/main/java/pro/sky/JavaHomework/services/BasketServiceImpl.java
@@ -0,0 +1,26 @@
+package pro.sky.JavaHomework.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+import pro.sky.JavaHomework.components.Basket;
+import pro.sky.JavaHomework.components.BasketImpl;
+
+import java.util.Set;
+
+@Service
+@SessionScope
+public class BasketServiceImpl implements BasketService {
+    private final Basket basket;
+
+    public BasketServiceImpl() {
+        this.basket = new BasketImpl();
+    }
+
+    public Set<Integer> getGoods() {
+        return basket.getGoods();
+    }
+
+    public void addGoods(Set<Integer> keys) {
+        keys.forEach(basket::addGoods);
+    }
+}
